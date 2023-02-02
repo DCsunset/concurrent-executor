1523ed398d1a216c96552833470447ece7551569
DCsunset <DCsunset@protonmail.com>
2023-02-02 12:09:48 -0500

    refactor: refactor code using functional programming style

--- cssh/executor.py
+++ cssh/executor.py
@@ -28,27 +28,24 @@ class SshExecutor:
 	Run ssh commands in parallel
 	"""
 	async def run(self, command: str):
-		awaitable_processes = []
-		for host in self.hosts:
-			p = asyncio.subprocess.create_subprocess_shell(
-				f"ssh {host} {self.sshOptions} -- {command}",
+		self.processes = await asyncio.gather(
+			# create subprocess for each host
+			*map(lambda h: asyncio.subprocess.create_subprocess_shell(
+				f"ssh {h} {self.sshOptions} -- {command}",
 				stdin=asyncio.subprocess.PIPE,
 				stdout=asyncio.subprocess.PIPE,
 				stderr=asyncio.subprocess.PIPE
-			)
-			awaitable_processes.append(p)
-
-		self.processes = await asyncio.gather(*awaitable_processes)
+			), self.hosts)
+		)
 		
 	"""
 	Wait for all commands to finish
 	Return a list of return codes
 	"""
 	async def wait(self) -> list[int]:
-		awaitables = [] 
-		for p in self.processes:
-			awaitables.append(p.wait())
-		return_codes = await asyncio.gather(*awaitables)
+		return_codes = await asyncio.gather(
+			*map(lambda p: p.wait(), self.processes)
+		)
 		return return_codes
 	
 	async def write_stdin(data):
@@ -60,24 +57,34 @@ class SshExecutor:
 		await asyncio.gather(*awaitables)
 	
 	"""
-	Get an async iterator to lines in stdout: (host, stdout_line)
+	Get a stream (async iterator) to lines in stdout: (host, stdout_line)
 	"""
 	def get_stdout(self):
-		stdouts = []
-		for i, p in enumerate(self.processes):
-			# must capture the current host by assigning it to h
-			stdouts.append(stream.map(p.stdout, lambda v, h=self.hosts[i]: (h, v)))
-		return stream.merge(*stdouts)
+		return stream.merge(
+			*map(
+				lambda h, p: stream.map(
+					p.stdout,
+					lambda v: (h, v)
+				),
+				self.hosts,
+				self.processes
+			)
+		)
 		
 	"""
 	Get an async iterator to lines in stderr: (host, stderr_line)
 	"""
 	def get_stderr(self):
-		stderrs = []
-		for i, p in enumerate(self.processes):
-			# must capture the current host by assigning it to h
-			stderrs.append(stream.map(p.stderr, lambda v, h=self.hosts[i]: (h, v)))
-		return stream.merge(*stderrs)
+		return stream.merge(
+			*map(
+				lambda h, p: stream.map(
+					p.stderr,
+					lambda v: (h, v)
+				),
+				self.hosts,
+				self.processes
+			)
+		)
 
 	"""
 	Terminate all commands
