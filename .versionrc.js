8e0f71236432197f714bcd9b63f44b61d779a001
DCsunset <DCsunset@protonmail.com>
2023-02-03 15:37:42 -0500

    chore: add version bnumper

---
+++ .versionrc.js
@@ -0,0 +1,25 @@
+const re = /^__version__ = "(\d\.\d\.\d)"/;
+
+function readVersion(contents) {
+	const matches = contents.match(re);
+	return matches[1];
+}
+
+function writeVersion(contents, version) {
+	return contents.replace(re, `__version__ = "${version}"`);
+}
+
+const tracker = {
+	filename: "cssh/_version.py",
+	updater: {
+		readVersion,
+		writeVersion
+	}
+};
+
+module.exports = {
+	// read version
+	packageFiles: [tracker],
+	// write version
+	bumpFiles: [tracker]
+};
